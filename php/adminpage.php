5df174e11e1be5d0ac749736f0ddfe9d92d20283
Darian Lio <14dl50@queensu.ca>
2018-03-28 01:21:49 -0400

    fixed bugs

--- php/adminpage.php
+++ php/adminpage.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
 <?php
 session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['admin'] == true) {
     echo "Hello " . $_SESSION['username'] . ".";
 } else {
     echo "Log in Please";
-}
+}?>
+
+</body>
+</html>
