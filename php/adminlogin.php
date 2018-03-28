e646940ad15b9a21515b74b16d6b66a4872c914c
seanblee <15sl18@queensu.ca>
2018-03-27 20:22:19 -0400

    admin login

--- php/adminlogin.php
+++ php/adminlogin.php
@@ -4,15 +4,15 @@ include_once 'dbconnect.php';
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-$sql="SELECT * FROM customer WHERE username='$username' and password='$password'";
+$sql="SELECT * FROM admin WHERE username='$username' and password='$password'";
 $result=$dbh->query($sql);
 $count=$result->rowCount();
 
 if($count==1){
     session_start();
-    echo "Logged in as " . $_SESSION['username'] . ".";
+    echo "Logged in as Administrator " . $_SESSION['username'] . ".";
     $_SESSION['loggedin'] = true;
-    $_SESSION['admin'] = false;
+    $_SESSION['admin'] = true;
     $_SESSION['username'] = $username;
 } else {
   echo "Fail";
--- php/adminpage.php
+++ php/adminpage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['admin'] == false) {
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['admin'] == true) {
     echo "Hello " . $_SESSION['username'] . ".";
 } else {
     echo "Log in Please";
