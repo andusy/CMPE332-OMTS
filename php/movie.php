5b3f0c26d4354708ad0367a262cb026641084359
Darian Lio <14dl50@queensu.ca>
2018-03-28 04:43:32 -0400

    added some css

--- php/movie.php
+++ php/movie.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-</head>
+		<!-- TITLE -->
+		<title>Movies</title>
 
+		<!-- FAVICON -->
+		<link rel="icon" type="image/png" sizes="16x16" href="../img/favicon-16x16.png" />
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+
+		<!-- ICONS -->
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" type=text/css>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/movie.css" />
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	</head>
 <body>
+  	<div class="content">
 	<h1>Movies</h1>
+	<hr>
 	<table>
 	<tr><th>Movie</th><th>Run Time (Minutes)</th><th>Plot Synopsis</th><th>Director</th><th>Production Company</th></tr>
 
@@ -23,7 +48,8 @@
 	?>
 	</table>
 	<h1>Purchase Tickets</h1>
-	<h2>Select Movie</h2>
+	<hr>
+		<label>Select Movie</label>
 	<form action="makepurchase.php" method="POST">
 	<select name = 'title' name = 'select'>
 		<option value = "">Select a movie...</option>
@@ -43,6 +69,6 @@
 	Number of Tickets: <input type = "text" name = "numTickets">
 	<button type="submit" class="btn btn-primary">See Showings</button>
 	</form>
-
+	</div>
  </body>
 </html>
